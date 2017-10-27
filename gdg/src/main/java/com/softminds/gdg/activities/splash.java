f57930085d19c2720909022be1c64db421a7680d
coder3101 <ashar786khan@gmail.com>
2017-10-28 00:25:45 +0530

    targetApi to 27, compileSdk to 27, splash UI init

---
+++ gdg/src/main/java/com/softminds/gdg/activities/splash.java
@@ -0,0 +1,15 @@
+package com.softminds.gdg.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.softminds.gdg.R;
+
+public class splash extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+    }
+}
