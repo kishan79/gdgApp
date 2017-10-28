a20a3e52d2e46a897e691dfc80b1a45da7bdbc8b
coder3101 <ashar786khan@gmail.com>
2017-10-28 12:07:09 +0530

    AppTheme Finalized

--- gdg/src/main/java/com/softminds/gdg/activities/splash.java
+++ gdg/src/main/java/com/softminds/gdg/activities/splash.java
@@ -1,5 +1,7 @@
 package com.softminds.gdg.activities;
 
+import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -7,9 +9,17 @@ import com.softminds.gdg.R;
 
 public class splash extends AppCompatActivity {
 
+    private static final long TIME_OUT = 1500;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(getApplicationContext(),MainActivity.class));
+            }
+        },TIME_OUT);
     }
 }
